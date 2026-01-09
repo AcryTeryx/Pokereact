d3de4e990fc8af7b37a60c13338e885a4da3b8c6
AcryTeryx <tlaucournet@gmail.com>
2026-01-09 09:16:11 +0100

    first commit from day 2 to branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import TrainerForm from './component/TrainerForm'
-
+import { Button } from '@mui/material'
 
 
 export default function App() {
   const [trainerList, setTrainerList] = useState([""])
   return (
     <>
-
+    <Button variant="contained"> G </Button>
     <h1> Hello world !</h1>
     <TrainerForm>
       
