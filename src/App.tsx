27023179ff1520825a35c3fdd3ccf5ec48d01835
AcryTeryx <tlaucournet@gmail.com>
2026-01-09 11:12:25 +0100

    Tailwind+router add, top right dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import TrainerForm from './component/TrainerForm'
 import { Button } from '@mui/material'
-
+import BasicMenu from './component/Menu'
 
 export default function App() {
   const [trainerList, setTrainerList] = useState([""])
   return (
     <>
+    <BasicMenu />
     <Button variant="contained"> G </Button>
-    <h1> Hello world !</h1>
-    <TrainerForm>
+    <h1> Pokereact</h1>
+    <h1 className="text-3xl font-bold text-white">
+    Hello world!
+  </h1>
+
+    {/* <TrainerForm/> */}
       
-    </TrainerForm>      
+    {/* </TrainerForm>       */}
     </>
   )
 }
